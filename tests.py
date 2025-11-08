1e5b05a07fa313e9d2e6f904238f804d971b5183
Galina Latonova <latonova.lina@yandex.ru>
2025-11-08 15:47:30 +0300

    удален ненужный блок if __name__

--- tests.py
+++ tests.py
@@ -1,29 +1,7 @@
 import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-        # напиши свои тесты ниже
-        # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-
     def test_add_new_book_success(self, books_collector):
         books_collector.add_new_book('Book1')
         assert 'Book1' in books_collector.books_genre , "Книга не добавлена в books_genre"
@@ -87,10 +65,6 @@ class TestBooksCollector:
         assert 'Book1' not in books_collector.favorites
 
     def test_get_list_of_favorites_books(self, books_collector):
-        # Проверяем получение списка избранных книг
         books_collector.add_new_book('Book1')
         books_collector.add_book_in_favorites('Book1')
         assert books_collector.get_list_of_favorites_books() == ['Book1']
-
-if __name__ == '__main__':
-    pytest.main()
